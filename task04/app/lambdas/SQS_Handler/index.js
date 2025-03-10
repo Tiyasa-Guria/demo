e08ea4d3bfbb69a5cdbeafe54d20e4579c6698d2
Tiyasa Guria <tiyasa_guria@epam.com>
2025-03-10 10:32:42 +0530

    committed

--- task04/app/lambdas/SQS_Handler/index.js
+++
@@ -1,8 +0,0 @@
-exports.handler = async (event) => {
-    event.Records.forEach(record => {
-        const messageBody = record.body;
-        console.log("Message received from SQS:", messageBody);
-    });
-
-    return { statusCode: 200 };
-};
